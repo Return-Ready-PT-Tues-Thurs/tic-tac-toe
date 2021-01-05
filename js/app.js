e925acfa982dcfa3f7a1c82a106a598343703b1a
Earl M. Smith <unixsa@gmail.com>
2021-01-05 16:56:27 -0500

    restart fix

--- js/app.js
+++ js/app.js
@@ -4,11 +4,9 @@ const restartBtn = document.getElementById('restartB');
 const spaces = [];
 const O_TEXT = "O";
 const X_TEXT = "X";
+const O_COLOR = "#ffa500";
+const X_COLOR = "#0000ff";
 let currentPlayer;
-
-//
-// Draw TicTacToe Board
-//
 
 const drawboard = () => {
     boxes.forEach((box, index) => {
@@ -29,27 +27,6 @@ const drawboard = () => {
         box.addEventListener('click', boxClicked) 
     });
 };
-//
-// Alternate Players color with Orange or Blue
-//
-
-function changeBackGround(id, currentPlayer){
-    let elem = document.getElementById(id);
-    let color = "";
-    let orange = "#ffa500"; 
-    let blue = "#2b7fb0";
-    if(currentPlayer === O_TEXT){
-       color = orange;
-       elem.style.background = color;
-    } else if(currentPlayer === X_TEXT){
-       color = blue; 
-       elem.style.background = color;
-    }
-  };
-
-//
-// Alternate Players with X or O
-//
 
 const boxClicked = (e) => { 
     const id = e.target.id;
@@ -58,59 +35,83 @@ const boxClicked = (e) => {
         e.target.innerText = currentPlayer;
       
         if(playerHasWon()){
-            playText.innerText = `${currentPlayer}  has won!`;
+            playText.innerText = `${currentPlayer} has won!`;
             return;
         }
         
         changeBackGround(id, currentPlayer);
-        currentPlayer = currentPlayer === O_TEXT ? X_TEXT: O_TEXT;
+        currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
     }
 };
 
-//
-// Winning Combinations
-//
+function changeBackGround(id, currentPlayer){
+  let elem = document.getElementById(id);
+  let color = "";
+  let orange = "#f1871b"; 
+  let blue = "#2b7fb0";
+  
+  if(currentPlayer === O_TEXT){
+     color = orange;
+     elem.style.background = color;
+  } else if(currentPlayer === X_TEXT){
+     color = blue; 
+     elem.style.background = color;
+  }
+}
+
+// document.getElementById(1).style.backgroundColor = X_COLOR
+// function changeColor(id){
+//     if (box.innerHTML ===''){
+//       if (turns ==='X'){
+//         turns='O';
+//         box.style.backgroundColor=X_COLOR;
+//       } else {
+//         turns='X';
+//         box.style.backgroundColor=O_COLOR;
+//       }
+//     }
+//   }
 
 const playerHasWon = () => {
     if(spaces[0] === currentPlayer){
         if(spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+            console.log(`${currentPlayer} wins up top`)
             return true;
         }
         if(spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+            console.log(`${currentPlayer} wins on the left`)
             return true;
         }
         if(spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+            console.log(`${currentPlayer} wins diagnally`)
             return true;
         }
     }
     if(spaces[8] === currentPlayer){
         if(spaces[2] === currentPlayer && spaces[5] === currentPlayer) {
+            console.log(`${currentPlayer} wins on the right`)
             return true;
         }
         if(spaces[6] === currentPlayer && spaces[7] === currentPlayer) {
+            console.log(`${currentPlayer} wins on the bottom`)
             return true;
         }
     }
     if(spaces[4] === currentPlayer){
         if(spaces[1] === currentPlayer && spaces[7] === currentPlayer) {
+            console.log(`${currentPlayer} wins vertically in the middle`)
             return true;
         }        
         if(spaces[3] === currentPlayer && spaces[5] === currentPlayer) {
+            console.log(`${currentPlayer} wins horizontally in the middle`)
             return true;
-        }  
-    }    
-    if(spaces[6] === currentPlayer){
+        }    
+    } if(spaces[6] === currentPlayer){
         if(spaces[4] === currentPlayer && spaces[2] === currentPlayer) {
             return true;
         }            
     }
 };
-
-
-//
-// Reset Gameboard
-//
-
 
 const restart = () => {
     spaces.forEach((space, index) => {
@@ -119,18 +120,10 @@ const restart = () => {
     boxes.forEach((box) => {
         box.innerText = '';
     });
-    boxes.forEach((box) => {
-        box.innerText = '';
-    });
-
-    // spaces[index] === null; {
-    //     color = "#f5f5f5";
-    //     elem.style.background = color;
-    // };
     playText.innerText = `Let's Play!`;
-    currentPlayer = O_TEXT;
+    currentPlayer = X_TEXT;
 }
 restartBtn.addEventListener('click', restart);
 restart();
-drawboard();
+drawboard(); 
 
